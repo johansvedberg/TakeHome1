e4816e7dd1009856b62c8bdf6c3821b9cf8202dc
johansvedberg <johan@svedberg.biz>
2017-04-27 23:33:24 +0200

    Most of Task 2 solved

--- src/task2/MainSimulation.java
+++ src/task2/MainSimulation.java
@@ -8,23 +8,27 @@ public class MainSimulation extends GlobalSimulation {
 
 	public static void main(String[] args) throws IOException {
 		Event actEvent;
-		State actState = new State(); // The state that shoud be used
+		State actState = new State(); // The state that should be used
 		// Some events must be put in the event list at the beginning
-		insertEvent(ARRIVAL1, 0);
-		insertEvent(MEASURE1, 5);
-		insertEvent(MEASURE2, 5);
+		insertEvent(ARRIVALA, 0);
+		insertEvent(MEASURE, 0);
 
 		// The main simulation loop
-		while (time < 1000) {
+		while (actState.nbrOfMeasurements < 1000) {
 			actEvent = eventList.fetchEvent();
 			time = actEvent.eventTime;
+		
 			actState.treatEvent(actEvent);
 		}
 
 		// Printing the result of the simulation, in this case a mean value
-		System.out.println("Mean value in Q2: " + 1.0 * actState.accumulated2 / actState.noMeasurements2);
-		System.out.println("Chance of rejection: " + 1.0 * actState.noRejected / actState.accumulated1);
-
+		// System.out.println("Mean value in Q2: " + 1.0 * actState.accumulated2
+		// / actState.noMeasurements2);
+		// System.out.println("Chance of rejection: " + 1.0 *
+		// actState.noRejected / actState.accumulated1);
+		System.out
+				.println("Mean number of jobs in the buffer: " + 1.0 * actState.inBuffer / actState.nbrOfMeasurements);
+		System.out.println(actState.nbrOfArrivals);
 		actState.W.close();
 	}
 }
---
+++ src/task2/State.java
@@ -0,0 +1,87 @@
+package task2;
+
+import java.util.*;
+import java.io.*;
+
+class State extends GlobalSimulation {
+
+	// Here follows the state variables and other variables that might be needed
+	// e.g. for measurements
+	public int inBuffer = 0, nbrOfMeasurements = 0, nbrOfArrivals = 0;
+
+	public LinkedList<Integer> buffer = new LinkedList<Integer>();
+
+	Random slump = new Random(); // This is just a random number generator
+	SimpleFileWriter W = new SimpleFileWriter("number.m", false);
+	final double arrivalTime = (1.0 / 150.0);
+	final double delay = 1.0;
+	final double serviceA = 0.002;
+	final double serviceB = 0.004;
+
+	// The following method is called by the main program each time a new event
+	// has been fetched
+	// from the event list in the main loop.
+	public void treatEvent(Event x) {
+		switch (x.eventType) {
+		case ARRIVALA:
+			arrivalA();
+			break;
+		case ARRIVALB:
+			arrivalB();
+			break;
+		case MEASURE:
+			measure();
+			break;
+		case READY:
+			ready();
+			break;
+		}
+	}
+
+	// The following methods defines what should be done when an event takes
+	// place. This could
+	// have been placed in the case in treatEvent, but often it is simpler to
+	// write a method if
+	// things are getting more complicated than this.
+
+	private void arrivalA() {
+		nbrOfArrivals++;
+		if (buffer.size() == 0)
+			insertEvent(READY, time + serviceA);
+
+		buffer.addLast(ARRIVALA);
+		insertEvent(ARRIVALA, time + expDistribution(arrivalTime));
+
+	}
+
+	private void arrivalB() {
+		if (buffer.size() == 0)
+			insertEvent(READY, time + serviceB);
+
+		buffer.addFirst(ARRIVALB);
+
+	}
+
+	private void ready() {
+		if (buffer.size() > 0) {
+			int inQueue = buffer.poll();
+			if (inQueue == ARRIVALA) {
+				insertEvent(READY, time + serviceA);
+				insertEvent(ARRIVALB, time + serviceA + delay);
+			} else if (inQueue == ARRIVALB) {
+				insertEvent(READY, time + serviceB);
+			}
+		}
+	}
+
+	private void measure() {
+		inBuffer = inBuffer + buffer.size();
+		nbrOfMeasurements++;
+		insertEvent(MEASURE, time + 0.1);
+
+	}
+
+	public double expDistribution(double mean) {
+		return -(mean) * Math.log(slump.nextDouble());
+	}
+}
